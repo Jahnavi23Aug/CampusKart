a814257699838c97c6d0d45ff425c8a81a775d94
Jahnavi Talari <talarijahnavi88@gmail.com>
2026-03-16 16:24:14 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -583,5 +583,8 @@ def pay_principal():
 if __name__ == "__main__":
     socketio.run(app, debug=True)
 
+import os
+
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
